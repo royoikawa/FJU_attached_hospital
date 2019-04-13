cdb2b7a3adb12a4ac0ab149d14ddc32248d219b0
林承鴻 <406401240@gapp.fju.edu.tw>
2019-04-13 13:10:49 +0800

    reservation.xml選擇科別 reservationimformation.xml使用者資料填寫 activity_main.xml 選單控制

--- app/src/main/java/first/com/MainActivity.java
+++ app/src/main/java/first/com/MainActivity.java
@@ -2,14 +2,22 @@ package first.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private String[] data={"胸腔內科","心臟內科","老年醫學科","神經內科","血液腫瘤科","腎臟內科","一般外科","神經外科","小兒科","整形外科","骨科","泌尿科"};
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.reservation);
+        //setContentView(R.layout.reservationimformation);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
+        ListView listview=(ListView)findViewById(R.id.frank);
+        listview.setAdapter(adapter);
 
     }
+
 }
