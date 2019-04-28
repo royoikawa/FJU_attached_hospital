93b0aa3bdb0e420cbddf8b6cd39461a4d1aaa07b
YuXian <leoleoleo980@gmail.com>
2019-04-28 22:43:46 +0800

    modify "view_clinic_number"

---
+++ app/src/main/java/first/com/viewclinicnumber.java
@@ -0,0 +1,21 @@
+package first.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class viewclinicnumber extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewclinicnumber);
+        Spinner spinner = (Spinner)findViewById(R.id.clinic_spinner);
+        ArrayAdapter<CharSequence> clinicList = ArrayAdapter.createFromResource(viewclinicnumber.this,
+                R.array.clinic,
+                android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(clinicList);
+    }
+}
+
