f7f64c7b7b08483827e3b3e18d0b5ccd6dd0c15e
leo <406401305@gapp.fju.edu.tw>
2019-04-30 18:51:13 +0800

    chooseoption 改版(全做完)

--- app/src/main/java/first/com/chooseoption.java
+++ app/src/main/java/first/com/chooseoption.java
@@ -7,12 +7,12 @@ import android.widget.ListView;
 
 public class chooseoption extends AppCompatActivity {
     //要先知道醫生上班日期,才能顯示選項
-    private String[] data={"4/18","4/22","4/24","4/26","4/27","4/29","4/30","5/1"};
+   // private String[] data={"4/18","4/22","4/24","4/26","4/27","4/29","4/30","5/1"};
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chooseoption);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(chooseoption.this,android.R.layout.simple_list_item_1,data);
+        /*ArrayAdapter<String> adapter=new ArrayAdapter<String>(chooseoption.this,android.R.layout.simple_list_item_1,data);
         ListView listview=(ListView)findViewById(R.id.frank);
-        listview.setAdapter(adapter);
+        listview.setAdapter(adapter);  */
     }
 }
