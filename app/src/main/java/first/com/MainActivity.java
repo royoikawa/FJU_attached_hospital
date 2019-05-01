f33b739147dac6fe9b5dd22f73c70e26018fef8c
leo <406401305@gapp.fju.edu.tw>
2019-05-01 19:23:05 +0800

    test chooseoption

--- app/src/main/java/first/com/MainActivity.java
+++ app/src/main/java/first/com/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.viewclinicnumber);
+        setContentView(R.layout.chooseoption);
 
         }
     }
