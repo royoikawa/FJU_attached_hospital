a6f030595ae4d433359933eb8960742e7fccd75a
YuXian <leoleoleo980@gmail.com>
2019-05-01 14:34:16 +0800

    註解不需要的spinner

--- app/src/main/java/first/com/viewclinicnumber.java
+++ app/src/main/java/first/com/viewclinicnumber.java
@@ -2,22 +2,22 @@ package first.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+/*import android.widget.ArrayAdapter;
+import android.widget.Spinner;*/
 
 
 public class viewclinicnumber extends AppCompatActivity {
-    private  Spinner spinner;
+    /*private  Spinner spinner;
     private ArrayAdapter<String> adapter;
-    private static final String[] Clinic={"泌尿科","骨科","皮膚科"};
+    private static final String[] Clinic={"泌尿科","骨科","皮膚科"};*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viewclinicnumber);
 
 
-        try{
-            spinner = (Spinner) findViewById(R.id.spinner01);
+       /* try{
+            spinner = (Spinner) findViewById(R.id.spinner);
             //將可選内容與ArrayAdapter連接起來
             adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,Clinic);
             //設置下拉列表的風格
@@ -26,7 +26,7 @@ public class viewclinicnumber extends AppCompatActivity {
             spinner.setAdapter(adapter);
         }catch(Exception e){
             e.printStackTrace();
-        }
+        }*/
     }
 }
 
