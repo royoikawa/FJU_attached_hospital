42b6616916540a465d177614e5f4afb3d35941f4
leo <406401305@gapp.fju.edu.tw>
2019-05-03 18:35:18 +0800

    建立button 連接

--- app/src/main/java/first/com/MainActivity.java
+++ app/src/main/java/first/com/MainActivity.java
@@ -1,6 +1,9 @@
 package first.com;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
 import android.os.Bundle;
 
 
@@ -9,8 +12,17 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.chooseoption);
-
+        setContentView(R.layout.activity_main);
+        Button nextPageBtn = (Button)findViewById(R.id.button12);
+        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this , userlogin.class);
+                startActivity(intent);
+            }
+        });
         }
+
     }
 
--- app/src/main/java/first/com/userlogin.java
+++ app/src/main/java/first/com/userlogin.java
@@ -1,7 +1,10 @@
 package first.com;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class userlogin extends AppCompatActivity {
@@ -10,5 +13,14 @@ public class userlogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlogin);
+        Button nextPageBtn = (Button)findViewById(R.id.button5);
+        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(userlogin.this , userreg.class);
+                startActivity(intent);
+            }
+        });
     }
 }
