181fc5f096b32f8144cdc47a47b8aab0b2fd7061
royliao <royliao88@gmail.com>
2019-05-17 19:18:46 +0800

    成功連線到資料並可以打印出id

--- app/src/main/java/first/com/MainActivity.java
+++ app/src/main/java/first/com/MainActivity.java
@@ -2,26 +2,70 @@ package first.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
+    private void loadData(){
+        String urlString = "https://api.airtable.com/v0/appgPqAWrw2xTWKdx/User?api_key=keyUwcLvTO51TNEHV";
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(urlString, new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode,Header[] headers, JSONObject response) {
+                Toast.makeText(getApplicationContext(),
+                        "Success!", Toast.LENGTH_LONG).show();
+                Log.d("Hot Text:", response.toString());
+                TextView dataview =(TextView)findViewById(R.id.dataview);
+                //String Json = response.toString();
+                try {
+                    JSONArray Array = response.getJSONArray("records");
+                    for(int i = 0; i<Array.length();i++){
+                        JSONObject userdata = Array.getJSONObject(i);
+                        String id = userdata.getString("id");
+                        dataview.append(id+"\n");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button nextPageBtn = (Button)findViewById(R.id.button12);
-        nextPageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this , userlogin.class);
-                startActivity(intent);
+            }
+
+
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject error) {
+                Toast.makeText(getApplicationContext(),
+                        "Error: " + statusCode + " " + e.getMessage(),
+                        Toast.LENGTH_LONG).show();
+                // Log error message
+                Log.e("Hot Text:", statusCode + " " + e.getMessage());
             }
         });
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_test);
+        loadData();
+        //Button nextPageBtn = (Button)findViewById(R.id.button12);
+        //nextPageBtn.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+          //  public void onClick(View v) {
+           //     Intent intent = new Intent();
+            //    intent.setClass(MainActivity.this , userlogin.class);
+             //   startActivity(intent);
+            //}
+        //});
         }
 
     }
