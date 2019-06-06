6a434d621bfe8246a60fe823ba94c86b71257a77
leo <406401305@gapp.fju.edu.tw>
2019-06-06 17:34:44 +0800

    chooseoption介面改版(已可選擇醫生與時間)

--- app/src/main/java/first/com/chooseoption.java
+++ app/src/main/java/first/com/chooseoption.java
@@ -24,6 +24,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -51,24 +52,29 @@ public class chooseoption extends AppCompatActivity {
     String option2="下週("+ini2+"~"+end2+")";
     //顯示醫生姓名
     ArrayList data = new ArrayList<String>();
+    ArrayList doctorname=new ArrayList<String>();
+    Spinner seldoc;
+    ListView timeview;
     String[] opening;
+    String nameoftime=null;
     String open="";
+    int clicknum=0;
     public void loadData(){
         String urlString = "https://api.airtable.com/v0/appgPqAWrw2xTWKdx/List of doctor?view=Grid%20view&api_key=keygkXy0a4GuCXh7p";
         final AsyncHttpClient client = new AsyncHttpClient();
         client.get(urlString, new JsonHttpResponseHandler() {
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                Log.d("Hot Text:", response.toString());
-                //ListView kindview =(ListView)findViewById(R.id.kindview);
-                //TextView tview =(TextView)findViewById(R.id.tview);
-                String Json = response.toString();
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        Log.d("Hot Text:", response.toString());
+                        //ListView kindview =(ListView)findViewById(R.id.kindview);
+                        //TextView tview =(TextView)findViewById(R.id.tview);
+                        String Json = response.toString();
                 try {
 
                     //顯示所選擇科別
                     TextView selectnum = (TextView)findViewById(R.id.selectnum);
                     Bundle bundle = getIntent().getExtras();
                     String selected_type = bundle.getString("name");
-                    selectnum.setText(selected_type+"醫生列表");
+                    selectnum.setText(selected_type);
                     //接所選order
                     String selected_num = bundle.getString("order");
 
@@ -79,7 +85,7 @@ public class chooseoption extends AppCompatActivity {
                         JSONObject fields = userdata.getJSONObject("fields");
                         String id = fields.getString("Divisions_number");
                         String name = fields.getString("Doctor_name");
-                        String nameoftime = fields.getString("Name_of_time");
+                       // nameoftime = fields.getString("Name_of_time");
                         /*if(selected_num.equals(id)){
                             String docname = fields.getString("Doctor_name");
                             Toast.makeText(getApplicationContext(),
@@ -93,21 +99,21 @@ public class chooseoption extends AppCompatActivity {
                         //列出所選擇科別的醫生
                         if(selected_num.equals(id)) {
                             LinearLayout ll = findViewById(R.id.testll);
-                            LinearLayout ll1 = new LinearLayout(chooseoption.this);
+                           /* LinearLayout ll1 = new LinearLayout(chooseoption.this);
                             ll1.setOrientation(LinearLayout.HORIZONTAL);
                             Resources res = getResources();
                             ll1.setBackgroundDrawable(res.getDrawable(R.drawable.rectangle));
-                            ll1.setMinimumHeight(450);
+                            ll1.setMinimumHeight(450);*/
                            // ll2.setDividerPadding(350);
                             //取得風格
                             //樣式
                             //ll2放圖
-                            LinearLayout ll2 = new LinearLayout(chooseoption.this);
+                            /*LinearLayout ll2 = new LinearLayout(chooseoption.this);
                             ll2.setOrientation(LinearLayout.HORIZONTAL);
-                            ll2.setMinimumWidth(200);ll2.setMinimumHeight(350);
+                            ll2.setMinimumWidth(200);ll2.setMinimumHeight(350);*/
                            //ll2.addView(tt);ll.addView(ll2);
                             //適當排版印出資料
-                            LinearLayout ll3=new LinearLayout(chooseoption.this);
+                            /*LinearLayout ll3=new LinearLayout(chooseoption.this);
                             ll3.setOrientation(LinearLayout.VERTICAL);
                             ll3.setMinimumWidth(400);
                             ll3.setMinimumHeight(350);
@@ -125,16 +131,9 @@ public class chooseoption extends AppCompatActivity {
                             tt.setPadding(20,40,0,0);
                             tt.setText("醫生姓名:"+name);
                             tt.setTextSize(17);
-                            ll4.addView(tt);ll3.addView(ll4);
-                            open+= nameoftime;
-                            //刪掉" "
-                            String open2 = open.replace("\"","");
-                            //刪掉[
-                            String open3 = open2.replace("[","");
-                            //刪掉]
-                            String open4 = open3.replace("]","");
-                            opening=open4.split(",");
-                            LinearLayout ll5=new LinearLayout(chooseoption.this);
+                            ll4.addView(tt);ll3.addView(ll4);*/
+                            doctorname.add(name);
+                            /*LinearLayout ll5=new LinearLayout(chooseoption.this);
                             ll5.setOrientation(LinearLayout.VERTICAL);
                             ll5.setMinimumWidth(1000);
                             ll5.setMinimumHeight(140);
@@ -143,26 +142,142 @@ public class chooseoption extends AppCompatActivity {
                             time.setWidth(200);time.setHeight(140);
                             time.setPadding(20,0,0,0);
                             time.setText("開診時段:");
-                            time.setTextSize(17);
-                            Spinner choosetime=new Spinner(chooseoption.this);
-                            choosetime.setMinimumWidth(20);choosetime.setMinimumHeight(70);
-                            choosetime.setPadding(250,0,0,0);
-                            final ArrayAdapter<String> timeList = new ArrayAdapter<>(chooseoption.this,android.R.layout.simple_spinner_dropdown_item,opening);
-                            choosetime.setAdapter(timeList);
-                            ll5.addView(time);ll5.addView(choosetime);ll3.addView(ll5);
-                            LinearLayout ll6=new LinearLayout(chooseoption.this);
+                            time.setTextSize(17);*/
+                           // Spinner choosetime=new Spinner(chooseoption.this);
+                            seldoc=findViewById(R.id.select_doc);
+                           // choosetime.setMinimumWidth(20);choosetime.setMinimumHeight(70);
+                           // choosetime.setPadding(250,0,0,0);
+                            final ArrayAdapter<String> nameList = new ArrayAdapter<>(chooseoption.this,android.R.layout.simple_spinner_dropdown_item,doctorname);
+                            seldoc.setAdapter(nameList);
+                            seldoc.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+                                @Override
+                                public void onItemSelected(AdapterView parent, View v, int position, long id) {
+                                    String n1=doctorname.get(position).toString();
+                                    Toast.makeText(getApplicationContext(),
+                                            "選"+n1,
+                                            Toast.LENGTH_LONG).show();
+                                    timeData(n1);
+
+
+                                    // parent = 事件發生的母體 spinner_items
+                                    // position = 被選擇的項目index = parent.getSelectedItemPosition()
+                                    // id = row id，通常給資料庫使用
+                                }
+                                @Override
+                                public void onNothingSelected(AdapterView parent) {}
+                            });
+
+
+                           }
+                       }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject error) {
+                Toast.makeText(getApplicationContext(),
+                        "Error: " + statusCode + " " + e.getMessage(),
+                        Toast.LENGTH_LONG).show();
+                // Log error message
+                Log.e("Hot Text:", statusCode + " " + e.getMessage());
+            }
+        });
+        }
+    public void timeData(final String getname){
+        String urlString = "https://api.airtable.com/v0/appgPqAWrw2xTWKdx/List of doctor?view=Grid%20view&api_key=keygkXy0a4GuCXh7p";
+        final AsyncHttpClient client = new AsyncHttpClient();
+        client.get(urlString, new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("Hot Text:", response.toString());
+                //ListView kindview =(ListView)findViewById(R.id.kindview);
+                //TextView tview =(TextView)findViewById(R.id.tview);
+                String Json = response.toString();
+                try {
+                    JSONArray Array = response.getJSONArray("records");
+                    for(int index=0;index<Array.length();index++){
+                        JSONObject userdata = Array.getJSONObject(index);
+                        JSONObject fields = userdata.getJSONObject("fields");
+                        String name = fields.getString("Doctor_name");
+                        if(getname.equals(name)){
+                            nameoftime = fields.getString("Name_of_time");
+                            open = nameoftime;
+                            //刪掉" "
+                            String open2 = open.replace("\"", "");
+                            //刪掉[
+                            String open3 = open2.replace("[", "");
+                            //刪掉]
+                            String open4 = open3.replace("]", "");
+                            opening = open4.split(",");
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(chooseoption.this, android.R.layout.simple_list_item_1, opening);
+                            timeview = (ListView) findViewById(R.id.timelist);
+                            timeview.setAdapter(adapter);
+                            timeview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView arg0, View arg1, int position, long arg3) {
+                                    Intent intent = new Intent();
+                                    intent.setClass(chooseoption.this, reservationimformation.class);
+                                    Bundle bundle = new Bundle();
+                                    String decided = opening[position];
+                                    String docname = getname;
+                                    bundle.putString("name", docname);
+                                    bundle.putString("time", decided);
+                                   // bundle.putString("order", position + 1 + "");
+                                    intent.putExtras(bundle);
+                                    startActivity(intent);
+                                }
+                            });
+                            break;
+                        }
+                    }
+
+
+
+                } catch (JSONException e) {
+
+                }
+
+            }
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject error) {
+                Toast.makeText(getApplicationContext(),
+                        "Error: " + statusCode + " " + e.getMessage(),
+                        Toast.LENGTH_LONG).show();
+                // Log error message
+                Log.e("Hot Text:", statusCode + " " + e.getMessage());
+            }
+        });
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chooseoption);
+        final String[] datedata={option1,option2};
+        Spinner spinner = findViewById(R.id.week);
+        final ArrayAdapter<String> dateList = new ArrayAdapter<>(chooseoption.this,android.R.layout.simple_spinner_dropdown_item,datedata);
+        spinner.setAdapter(dateList);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(chooseoption.this,"您選擇的月份是："+dateList.getItem(position),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        loadData();
+        //ll5.addView(time);ll5.addView(choosetime);ll3.addView(ll5);
+                           /* LinearLayout ll6=new LinearLayout(chooseoption.this);
                             ll6.setOrientation(LinearLayout.VERTICAL);
                            ll6.setMinimumWidth(260); ll6.setMinimumHeight(100);
-                           ll6.setPadding(510,0,0,0);
-                            Button getoption=new Button(chooseoption.this);
+                           ll6.setPadding(510,0,0,0);*/
+                           /* Button getoption=new Button(chooseoption.this);
                             getoption.setMinimumWidth(230);getoption.setMinimumHeight(100);
                             getoption.setBackgroundColor(Color.RED);
                             getoption.setPadding(10,0,0,0);
                             getoption.setText("預約");
                             getoption.setTextSize(16);
                             getoption.setTextColor(Color.WHITE);
-                            ll6.addView(getoption);
-                            ll3.addView(ll6);ll1.addView(ll3);ll.addView(ll1);
+                            ll6.addView(getoption);*/
                            /* LinearLayout ll4=new LinearLayout(chooseoption.this);
                             ll4.setOrientation(LinearLayout.HORIZONTAL);
                             ll4.setMinimumHeight(100);
@@ -187,54 +302,21 @@ public class chooseoption extends AppCompatActivity {
                             ll2.addView(ll5);*/
 
 
-                            //LinearLayout ll3=new LinearLayout(chooseoption.this);
-                            //ll3.setOrientation(LinearLayout.HORIZONTAL);
+        //LinearLayout ll3=new LinearLayout(chooseoption.this);
+        //ll3.setOrientation(LinearLayout.HORIZONTAL);
                            /* TextView timetext = new TextView(chooseoption.this);
                             timetext.setWidth(1200);timetext.setHeight(100);
                             timetext.setPadding(500,30,0,0);
                             timetext.setText("開診時段:");
                             ll3.addView(timetext);
                             ll.addView(ll3);*/
-                        }
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject error) {
-                Toast.makeText(getApplicationContext(),
-                        "Error: " + statusCode + " " + e.getMessage(),
-                        Toast.LENGTH_LONG).show();
-                // Log error message
-                Log.e("Hot Text:", statusCode + " " + e.getMessage());
-            }
-        });
-        }
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.chooseoption);
-        final String[] datedata={option1,option2};
-        Spinner spinner = findViewById(R.id.week);
-        final ArrayAdapter<String> dateList = new ArrayAdapter<>(chooseoption.this,android.R.layout.simple_spinner_dropdown_item,datedata);
-        spinner.setAdapter(dateList);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //Toast.makeText(chooseoption.this,"您選擇的月份是："+dateList.getItem(position),Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-        loadData();
-
 
 
 
         /*ArrayAdapter<String> adapter=new ArrayAdapter<String>(chooseoption.this,android.R.layout.simple_list_item_1,data);
         ListView listview=(ListView)findViewById(R.id.frank);
         listview.setAdapter(adapter);  */
+        //點選時段換頁
+
     }
         }
