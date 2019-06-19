6e8cde805da8f7568274cedc0530a037ada702b2
royliao <royliao88@gmail.com>
2019-06-19 17:43:37 +0800

    註冊驗證完成

--- app/src/main/java/first/com/MyAPIService.java
+++ app/src/main/java/first/com/MyAPIService.java
@@ -1,17 +1,20 @@
 package first.com;
 
 
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
 
 public interface MyAPIService {
     @GET("User?api_key=keyUwcLvTO51TNEHV")
     Call<records> getRecords();
-
     @POST("User?api_key=keyUwcLvTO51TNEHV")
         // 用@Body表示要傳送Body資料
     Call<records> postRecords(@Body userPost fields);
